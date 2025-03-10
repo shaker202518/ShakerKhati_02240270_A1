841368c9d21fd90c0ab4d26a6de7fac2c9bd3f1c
Shaker khati <0220270.cst@rub.edu.bt>
2025-03-11 00:25:11 +0530

    my A1 second commit

--- ShakerKhati_02240270_A1_PA.py
+++ ShakerKhati_02240270_A1_PA.py
@@ -1,4 +1,14 @@
 def is prime(n):
-    if not isinstance(n, int)or n <= 1:
-        return False 
-    return all (n % i != 0 fot i in range (2, int(n**0.5) + 1))
+    if n < 2:
+        return false
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def sum_of_peimes(start, end):
+    try:
+        start, end = int(start), int(end)
+        if start > end:
+            start, end = end, start
+            primes_sum = sum(num for num in range(start, end + 1) if is _primes(num))
